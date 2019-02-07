f9ea8cca70fb97439e3439f44f2ad5abd5171bdc
xiluhua <xlh505440@163.com>
2019-02-07 09:52:04 +0800

    update logback-spring.xml

--- src/main/java/com/springBoot/SpringBoot03Application.java
+++ src/main/java/com/springBoot/SpringBoot03Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource(value = {"classpath:spring.xml"})
+@ImportResource(value = {"classpath:applicationContext.xml"})
 @SpringBootApplication
 public class SpringBoot03Application {
 
